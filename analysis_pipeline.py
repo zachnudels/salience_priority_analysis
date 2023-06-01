d1df362770cd780ef0356b79ca5f336cdafde148
Zach <zachnudels@gmail.com>
2023-06-01 16:40:25 +0200

    Simplified Analysis Settings
    Removed the need to indicate which participants to run. And now the
    pipeline runs on multiple dependent variables simultaneously.

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -32,31 +32,21 @@ def construct_balance_tables():
     SOA_df.to_csv(os.path.join(params['directories']['tables'], "SOA_balances.csv"))
 
 
-def check_subject_parity(included_subjects, excluded_participants, final_participants):
-    for excluded_participant in excluded_participants:
-        included_subjects.remove(excluded_participant)
+def main(params):
+    subjects = unzipData(params['directories']['root'])
+    params['participants'] = subjects
+    parseEyeTrackerData(params['directories']['root'], params['directories']['parsed_data'])
+    excluded_participants = cleanData(params)
+    extractData(params, excluded_participants)
+    plot(params)
+    construct_balance_tables()
 
-    if included_subjects != final_participants:
-        warnings.warn(f" \n\n WARNING!!! \nThe specified participants to use in analysis_settings does not correspond"
-                      f" with the files found in the specified root folder."
-                      f"\nIn the directory: {included_subjects}. \nIn the settings: {final_participants}"
-                      f"\nPlease make sure you are using the correct participants in analysis_settings.yml."
-                      f"\n WARNING!!! \n\n", category=Warning)
 
     if len(excluded_participants) > 0:
         warnings.warn(f" \n\n WARNING!!! \n The following participants were excluded for having less "
                       f"than 50% valid trials: \n{excluded_participants}"
                       f"\n WARNING!!! \n\n", category=Warning)
 
-
-def main(params):
-    subjects = unzipData(params['directories']['root'])
-    parseEyeTrackerData(params['directories']['root'], params['directories']['parsed_data'])
-    excluded_participants = cleanData(params)
-    final_participants = extractData(params, excluded_participants)
-    plot(params)
-    construct_balance_tables()
-    check_subject_parity(subjects, excluded_participants, final_participants)
 
 
 
